c67531fda18a6408cbd443c380344b185a2fba2b
hitesh <h.kankarwal@gmail.com>
2023-04-13 17:52:46 +0530

    Exports lib in index.ts

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,5 @@
 export { run } from '@oclif/core';
+export { runInit } from './lib/init';
+export { runDeploy } from './lib/deploy';
+export { createSubgraph } from './lib/create';
+export { removeSubgraph } from './lib/remove';
